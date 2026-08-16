7f1621ca389152512603a00ca553c5ffb01e25f0
egegokdenizduman <egegokdenizduman@gmail.com>
2026-08-16 05:08:16 +0300

    remove everycompat compat and barrel recepies

--- src/main/java/com/jesz/createdieselgenerators/CreateDieselGenerators.java
+++ src/main/java/com/jesz/createdieselgenerators/CreateDieselGenerators.java
@@ -1,13 +1,10 @@
 package com.jesz.createdieselgenerators;
 
-import com.cake.struts.StrutYourStuff;
-import com.jesz.createdieselgenerators.compat.EveryCompatCompat;
 import com.jesz.createdieselgenerators.compat.computercraft.CCProxy;
 import com.jesz.createdieselgenerators.compat.strut_your_stuff.StrutYourStuffRegistryEntries;
 import com.jesz.createdieselgenerators.content.molds.MoldType;
 import com.jesz.createdieselgenerators.content.tools.lighter.LighterModel;
 import com.jesz.createdieselgenerators.packets.CDGPackets;
-import com.jesz.createdieselgenerators.ponder.CDGPonderPlugin;
 import com.simibubi.create.compat.Mods;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.item.ItemDescription;
@@ -15,7 +12,6 @@ import com.simibubi.create.foundation.item.KineticStats;
 import com.simibubi.create.foundation.item.TooltipModifier;
 import net.createmod.catnip.lang.FontHelper;
 import net.createmod.catnip.platform.CatnipServices;
-import net.createmod.ponder.foundation.PonderIndex;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -25,7 +21,6 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
 import static com.jesz.createdieselgenerators.CreateDieselGenerators.ID;
 
@@ -59,8 +54,6 @@ public class CreateDieselGenerators
         if (ModList.get().isLoaded("struts"))
             StrutYourStuffRegistryEntries.register();
 
-        if (ModList.get().isLoaded("moonlight"))
-            EveryCompatCompat.init();
         Mods.COMPUTERCRAFT.executeIfInstalled(() -> CCProxy::register);
 
         CatnipServices.PLATFORM.executeOnClientOnly(() -> () -> onClient(modEventBus, container));
